a94f7fc570f056262fa74a2a632d6fad42afc147
RobynR <robynray59@gmail.com>
2022-12-11 17:02:39 -0600

    Changes to be committed: 	modified:   scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -22,9 +22,9 @@ function validateInput(testInput) {
     // list.style.visibility = "hidden"
     if (testInput === "") {
         return "Empty";
-    } else if (Number.isNaN(testInput)) {
+    } else if (isNaN(testInput)) {
         return "Not a Number";
-    } else if (!Number.isNaN(testInput)) {
+    } else if (!isNaN(testInput)) {
         return "Is a Number";
     }
 
@@ -46,7 +46,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
 
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
-       window.alert("Enter valid input for each field"); {
+        window.alert("Enter valid input for each field"); {
             //     list.style.visibility = "hidden"
             // 
         }
